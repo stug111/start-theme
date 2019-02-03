8745a6f760b1f6799df2e8e11c0765dcd3ca412f
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-02-03 11:06:17 +0700

    Init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Theme Sass.
+import './sass/style.scss';
+
+// Theme JavaScript.
+import './js/customizer';
+import './js/navigation';
+import './js/skip-link-focus-fix';
+import './js/common';
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+  BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
+  StyleLintPlugin = require('stylelint-webpack-plugin'),
+  SpriteLoaderPlugin = require('svg-sprite-loader/plugin'),
+  FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    frontend: ['babel-polyfill', './src/index.js'],
+    customizer: './src/customizer.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name]-bundle.js'
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /node_modules/,
+        test: /\.jsx$/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true,
+          spriteFilename: 'svg-defs.svg'
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images/',
+              name: '[name].[ext]'
+            }
+          },
+          'img-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new StyleLintPlugin(),
+    new MiniCssExtractPlugin({ filename: '../style.css' }),
+    new SpriteLoaderPlugin(),
+    new FriendlyErrorsWebpackPlugin(),
+    new BrowserSyncPlugin({
+      files: ['**/*.php', '**/*.scss'],
+      injectChanges: true,
+      proxy: 'http://ecotransport.local'
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJSPlugin(), new OptimizeCssAssetsPlugin()]
+  }
+};
